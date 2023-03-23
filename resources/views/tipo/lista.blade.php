eaecda70b1b2f484652495b428e78d09126cae41
Ariel Guareschi <arielguareschi@gmail.com>
2023-03-23 09:11:44 -0300

    adjustments

--- resources/views/tipo/lista.blade.php
+++ resources/views/tipo/lista.blade.php
@@ -1,56 +1,54 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-header">
-                    Lista de Tipos
-                    <a href="{{ url('tipo/create') }}"
-                        class="btn btn-success btn-sm float-end">
-                        Novo Tipo
-                    </a>
-                </div>
-                <div class="card-body">
-                    <table class="table table-sm table-hover table-bordered">
-                        <thead>
-                            <tr>
-                                <th>Código</th>
-                                <th>Titulo</th>
-                                <th>Opções</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($tipos as $tipo)
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Lista de Tipos
+                        <a href="{{ url('tipo/create') }}" class="btn btn-success btn-sm float-end">
+                            Novo Tipo
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-sm table-hover table-bordered">
+                            <thead>
                                 <tr>
-                                    <td>{{ $tipo->id }}</td>
-                                    <td>{{ $tipo->titulo }}</td>
-                                    <td>
-                                        <a href="{{ url('tipo/'.$tipo->id) }}"
-                                            class="btn btn-primary btn-sm">
-                                            Editar
-                                        </a>
-                                        {!! Form::open([
-                                            'method'=>'DELETE',
-                                            'url'=>'tipo/'.$tipo->id,
-                                            'style'=>'display:inline']) !!}
-                                            <button type="submit"
-                                                class="btn btn-danger btn-sm">
+                                    <th>Código</th>
+                                    <th>Titulo</th>
+                                    <th>Opções</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($tipos as $tipo)
+                                    <tr>
+                                        <td>{{ $tipo->id }}</td>
+                                        <td>{{ $tipo->titulo }}</td>
+                                        <td>
+                                            <a href="{{ url('tipo/' . $tipo->id) }}" class="btn btn-primary btn-sm">
+                                                Editar
+                                            </a>
+                                            {!! Form::open([
+                                                'method' => 'DELETE',
+                                                'url' => 'tipo/' . $tipo->id,
+                                                'style' => 'display:inline',
+                                            ]) !!}
+                                            <button type="submit" class="btn btn-danger btn-sm">
                                                 Excluir
                                             </button>
-                                        {!! Form::close() !!}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                    <div class="pagination justify-content-center">
-                        {{ $tipos->links() }}
+                                            {!! Form::close() !!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                        <div class="pagination justify-content-center">
+                            {{ $tipos->links() }}
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
